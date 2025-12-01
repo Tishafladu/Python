ca398a497d15dd32d587ca65a1d1577f371c19c7
Tisha Faldu <faldutisha95@gmail.com>
2025-12-01 15:48:22 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,28 @@
+#Task 1: Perform Basic Mathematical Operations
+
+n1 = input("Enter the first number: ")
+n2 = input("Enter the second number: ")
+
+a = int(n1)+int(n2)
+print("Addition :",a)
+
+a = int(n1)-int(n2)
+print("Subtration :",a)
+
+a = int(n1)*int(n2)
+print("Multiplication :",a)
+
+a = int(n1)/int(n2)
+print("Division :",a)
+print("Hello,", full_name + "! Welcome to the Python programming world.")
+ 
+
+#Task 2: Create a Personalized Greeting
+
+first_name = input("Enter the first name: ")
+last_name = input("Enter the last name: ")
+
+full_name = first_name + " " + last_name
+
+print("Hello,", full_name + "! Welcome to the Python programming world.")
+ 
